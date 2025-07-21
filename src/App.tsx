478b24090ed067f09e83fed60c87f7a9ddb6d472
tarun-etikala <etikala.t@northeastern.edu>
2025-07-21 13:03:17 -0400

    default mode change

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
-    <ThemeProvider defaultTheme="dark" attribute="class">
+    <ThemeProvider defaultTheme="light" attribute="class">
       <TooltipProvider>
         <ThemeToggle />
         <Toaster />
